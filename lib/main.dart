91de77ec3068bdfff128b766afcf631a5c9a74b9
Manar-Emad75 <manaremad904@gmail.com>
2023-09-04 08:09:36 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/ThemeProvider/theme_provider.dart';
 import 'package:flutter_project/layout/home_layout.dart';
 import 'package:flutter_project/modules/account/account_screen.dart';
 import 'package:flutter_project/modules/details/details_screen.dart';
@@ -8,10 +9,12 @@ import 'package:flutter_project/modules/home/home_screen.dart';
 import 'package:flutter_project/modules/signin/signin_screen.dart';
 import 'package:flutter_project/modules/signup/signup_screen.dart';
 import 'package:flutter_project/modules/splash/splash_screen.dart';
+import 'package:flutter_project/modules/welcome/welcome.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 Widget start (){
@@ -26,12 +29,17 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeLayout(),
-    );
-
-  }
+  Widget build(BuildContext context) =>ChangeNotifierProvider(
+      create: (context)=>ThemeProvider(),
+      builder: (context,_){
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          themeMode: themeProvider.themeMode,
+          darkTheme: MyTheme.darkTheme,
+          theme: MyTheme.lightTheme,
+          home: splash(),
+        );
+      }
+  );
 }
-
